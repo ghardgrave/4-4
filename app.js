8268eff7ab6e3fbea8d367cde017d21b8e335d83
ghardgrave <ghardgrave@gmail.com>
2016-05-22 16:40:10 -0700

    revised currency app

--- app.js
+++ app.js
@@ -1,63 +1,60 @@
 
 $(function(){
-      getRequest();
+
+
+/* displays current exchange*/
+showLive();
+  
+$('#datepicker').datepicker({
+    dateFormat: "yy-mm-dd",
+    inline : true,
+    altField : '#hiddenFieldID',
+    onSelect : function(){
+        var olddate = $("#datepicker").datepicker({ dateFormat: 'dd,MM,yyyy' }).val();
+        console.log(olddate);
+        getRequest(olddate);
+    }
 });
 
 
-function getRequest() {
-
-var auth = {
-             
-                consumerKey : "flINaTdzYOcnVhg_o4HxKQ",
-                consumerSecret : "3vSMRGbweP2eKouZs7LG0ADBjoo",
-                accessToken : "ykDPbThrMozb-oG-36ETSksJ3gDNtFrn",
-                accessTokenSecret : "dtLQD6jiQKbIqQ1pXYaTK2wGSoA",
-                serviceProvider : {
-                    signatureMethod : "HMAC-SHA1"
-                }
-            };
-
-            var terms = 'food';
-            var near = 'San+Francisco';
-
-            var accessor = {
-                consumerSecret : auth.consumerSecret,
-                tokenSecret : auth.accessTokenSecret
-            };
-            parameters = [];
-            parameters.push(['term', terms]);
-            parameters.push(['location', near]);
-            parameters.push(['callback', 'cb']);
-            parameters.push(['oauth_consumer_key', auth.consumerKey]);
-            parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
-            parameters.push(['oauth_token', auth.accessToken]);
-            parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
-
-            var message = {
-                'action' : 'http://api.yelp.com/v2/search',
-                'method' : 'GET',
-                'parameters' : parameters
-            };
-
-            OAuth.setTimestampAndNonce(message);
-            OAuth.SignatureMethod.sign(message, accessor);
-
-            var parameterMap = OAuth.getParameterMap(message.parameters);
-            console.log(parameterMap);
-
-            $.ajax({
-                'url' : message.action,
-                'data' : parameterMap,
-                'dataType' : 'jsonp',
-                'jsonpCallback' : 'cb',
-                'success' : function(data, textStats, XMLHttpRequest) {
-                    console.log(data);
-                    //$("body").append(output);
-                }
-            });
+});   
+     
 
 
+
+function showLive(){
+// set endpoint and your access key
+
+
+// get the most recent exchange rates via the "live" endpoint:
+  $.ajax({
+    url: 'http://apilayer.net/api/live?access_key=bd09618e70168a850cdd4823ad353816',   
+    dataType: 'jsonp',
+    success: function(json) {
+
+    $(".live").append("<p>"+json.quotes.USDTWD+"</p>");
+
+    }
+  });
+
+}
+
+
+function getRequest(olddate) {
+
+$.ajax({
+    url: 'http://apilayer.net/api/historical?access_key=bd09618e70168a850cdd4823ad353816&date='+olddate,   
+    dataType: 'jsonp',
+    success: function(json) {
+
+    $(".histrate").html("<p>"+json.quotes.USDTWD+"</p>");
+
+        
+    }
+})
+}           
+
+function showResults(results){
  
 }
 
-            
